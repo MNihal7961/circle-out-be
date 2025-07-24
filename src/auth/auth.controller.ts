df85c9cc479b16fe66d678aaadaa0fb5de6228d1
MNihal7961 <mnihal7961@gmail.com>
2025-07-24 18:16:24 +0530

    Add AuthController for user authentication endpoints including signup, signin, refresh, and user retrieval. Implement corresponding unit tests for AuthController functionality.

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,32 @@
+import { Body, Controller, Post, Req, Get } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { SetMetadata } from '@nestjs/common';
+import { Public } from 'src/utils/public.auth.decorator';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Public()
+  @Post('signup')
+  async signup(@Body() body: { email: string; password: string }) {
+    return this.authService.signup(body.email, body.password);
+  }
+
+  @Public()
+  @Post('signin')
+  async signin(@Body() body: { email: string; password: string }) {
+    return this.authService.signin(body.email, body.password);
+  }
+
+  @Public()
+  @Post('refresh')
+  async refresh(@Body() body: { userId: string; refreshToken: string }) {
+    return this.authService.refresh(body.userId, body.refreshToken);
+  }
+
+  @Get('me')
+  async me(@Req() req) {
+    return req.user;
+  }
+} 
